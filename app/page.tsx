2e421ecf8e8dcac2271017e0eb4a727b2c919c1c
FarBump <jefriamharallid@gmail.com>
2025-12-30 11:51:09 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -481,17 +481,3 @@ export default function BumpBotDashboard() {
     </div>
   )
 }
-
-import { useSmartWallets } from "@privy-io/react-auth/smart-wallets";
-
-export function WalletDisplay() {
-  const { smartWallets } = useSmartWallets();
-  const smartWallet = smartWallets[0];
-  if (!smartWallet) return <p>Sedang membuat Smart Wallet...</p>;
-  return (
-    <div>
-      <p>Smart Wallet Address: {smartWallet.address}</p>
-      <p>Type: {smartWallet.smartAccountType}</p> {/* Harusnya 'light-account' atau 'kernel' */}
-    </div>
-  );
-}
